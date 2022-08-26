bd9c08713fc68fec08939885a29f2de662dd7c5b
dlaw96@hotmail.com <dlaw96@hotmail.com>
2022-08-26 08:46:54 -0400

    created first routing

--- backend/src/main/java/ca/sheridancollege/backend/Controllers/MainController.java
+++ backend/src/main/java/ca/sheridancollege/backend/Controllers/MainController.java
@@ -1,13 +1,26 @@
 package ca.sheridancollege.backend.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.sheridancollege.backend.Model.Analyst;
+import ca.sheridancollege.backend.Service.AnalystService;
+
 @RestController
+@RequestMapping("/api/v1/")
+@CrossOrigin("*")
 public class MainController {
   
+  @Autowired
+  private AnalystService analystService;
+
   @GetMapping("/")
-  public String index(){
-    return null;
+  public List<Analyst> getAllAnalysts(){
+    return analystService.getAllAnalysts();
   }
 }
