542bdf66e288371bce00dfabdae9eca690f08fd1
dlaw96@hotmail.com <dlaw96@hotmail.com>
2022-08-26 22:19:05 -0400

    Added routes for CRUD functionality

--- backend/src/main/java/ca/sheridancollege/backend/Controllers/MainController.java
+++ backend/src/main/java/ca/sheridancollege/backend/Controllers/MainController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +23,26 @@ public class MainController {
   @Autowired
   private AnalystService analystService;
 
-  @GetMapping("/")
+  @GetMapping("/getAll")
   public List<Analyst> getAllAnalysts(){
     return analystService.getAllAnalysts();
+  }
+
+  @GetMapping("/add")
+  public String addAnalyst(@RequestBody Analyst analyst){
+    analystService.saveAnalyst(analyst);
+    return "New employee is added";
+  }
+
+  @DeleteMapping("/delete/{id}")
+  public String deleteAnalyst(@PathVariable Long id){
+    analystService.deleteAnalyst(id);
+    return "Deleted employee";
+  }
+
+  @PutMapping("/update/{id}")
+  public String updateAnalyst(@PathVariable Long id, @RequestBody Analyst analystDetails){
+    analystService.updateAnalyst(analystDetails, id);
+    return "Updated Employee";
   }
 }
